36ffef6eba8f187111ea7ea2d2dd332584fdf559
Pawel Dyjak <pawel.dyjak@poczta.onet.pl>
2025-10-21 18:24:32 +0200

    sql updated

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -16,3 +16,28 @@ VALUES (1, 5, 5),
        (4, 10, 3),
        (2, 8, 8),
        (3, 6, 4);
+
+DROP TABLE IF EXISTS authorities;
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users (
+    username VARCHAR(50) NOT NULL PRIMARY KEY,
+    password CHAR(68) NOT NULL,
+    enabled BOOLEAN NOT NULL
+);
+
+INSERT INTO users (username, password, enabled) VALUES
+('customer','{bcrypt}$2a$12$8BY94v4.yGezkjmXAngXDuEWedf8jddnsT1PExnikMX9fBMjvyyS2', TRUE),
+('admin','{bcrypt}$2a$12$f./H9Q16sFSN//eMTuGiWepilHud8JJqq1whXgxNNcGu2bgfMuWE2', TRUE);
+
+CREATE TABLE authorities (
+    username VARCHAR(50) NOT NULL,
+    authority VARCHAR(50) NOT NULL,
+    CONSTRAINT pk_authorities PRIMARY KEY (username, authority),
+    CONSTRAINT fk_user FOREIGN KEY (username) REFERENCES users(username)
+);
+
+INSERT INTO authorities (username, authority) VALUES
+('customer','ROLE_CUSTOMER'),
+('admin','ROLE_CUSTOMER'),
+('admin','ROLE_ADMIN');
